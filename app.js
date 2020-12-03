5654b83890e9480faeb46f9a7c4ce0d111edb790
DeepakYadav <42115559+deepak-07@users.noreply.github.com>
2020-12-03 15:25:36 +0530

    feat: api added

---
+++ app.js
@@ -0,0 +1,30 @@
+// alert("Connected");
+var input = document.querySelector("#input-txt");
+var btnClick = document.querySelector("#btn");
+var otpt = document.querySelector(".output");
+// var server = "https://api.funtranslations.com/translate/binary.json";
+var server = "https://api.funtranslations.com/translate/binary-to-text.json";
+
+
+function getTranslateURL(txt) {
+    return server + "?" + "text=" + txt;
+}
+
+function errorHandler(error) {
+    console.log("Error occured", error);
+    alert("Something wrong with the server!! Try again after sometime ");
+}
+
+function clickHandler() {
+    var inputTxt = input.value;
+    fetch(getTranslateURL(inputTxt))
+        .then(response => response.json())
+        .then(json => {
+            var translatedTxt = json.contents.translated;
+            otpt.innerText = translatedTxt;
+
+        })
+        .catch(errorHandler)
+};
+
+btnClick.addEventListener("click", clickHandler);
