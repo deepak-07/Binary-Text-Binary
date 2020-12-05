968dd932d1164294867516a4a22f89978fdbae0e
DeepakYadav <42115559+deepak-07@users.noreply.github.com>
2020-12-05 19:00:00 +0530

    upadte

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 // alert("Connected");
 var input = document.querySelector("#input-txt");
 var btnClick = document.querySelector("#btn");
-var btnClickTwo = document.querySelector("#btn-two");
+
 var otpt = document.querySelector(".output");
 var server = "https://api.funtranslations.com/translate/binary.json"
-    var servertwo = "https://api.funtranslations.com/translate/binary-to-text.json"
+
 
 
 function getTranslateURL(txt) {
     return server + "?" + "text=" + txt;
 }
 
-function getTranslateURLtwo(txt) {
-    return servertwo + "?" + "text=" + txt;
-}
+
 
 function errorHandler(error) {
     console.log("Error occured", error);
@@ -32,18 +30,4 @@ function clickHandler() {
         .catch(errorHandler)
 };
 
-function clickHandlerTwo() {
-    var inputTxt = input.value;
-    fetch(getTranslateURLtwo(inputTxt))
-        .then(response => response.json())
-        .then(json => {
-            var translatedTxt = json.contents.translated;
-            otpt.innerText = translatedTxt;
-
-        })
-        .catch(errorHandler)
-};
-
 btnClick.addEventListener("click", clickHandler);
-
-btnClickTwo.addEventListener("click", clickHandlerTwo);
